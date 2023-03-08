3cafbe035f7f03145f0835d37c7e87ed101201e8
Marta <rishnyakmarta@gmail.com>
2023-03-08 15:00:11 +0200

    config

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,6 +1,6 @@
-import {model, Schema} from "mongoose";
+import { model, Schema } from "mongoose";
 
-import {EGenders} from "../types";
+import { EGenders } from "../types";
 
 const userSchema = new Schema(
   {
