31a46178b62d993b2182e163b25793b66e48d8af
Marta <rishnyakmarta@gmail.com>
2023-03-08 14:44:28 +0200

    config

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -3,6 +3,6 @@ import {config} from "dotenv";
 config();
 
 export const configs = {
-    PORT: process.env.PORT || 5000,
-    DB_URL: process.env.DB_URL || " ",
+  PORT: process.env.PORT || 5000,
+  DB_URL: process.env.DB_URL || " ",
 };
