338e019a58b325084996714c92c5591ed9ee4294
Marta <rishnyakmarta@gmail.com>
2023-03-11 20:35:47 +0200

    config

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -4,5 +4,5 @@ config();
 
 export const configs = {
   PORT: process.env.PORT || 5000,
-  DB_URL: process.env.DB_URL || "mongodb://localhost:27017/test",
+  DB_URL: process.env.DB_URL || " ", //your db_url
 };
